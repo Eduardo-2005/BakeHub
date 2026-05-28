e96c18bdaf65d5ccdd4da1a5d523fd5bcf7491a3
Eduardo Meza <eduardome3105@gmail.com>
2026-05-28 07:21:55 -0600

    Corregir index.html y otros cambios

---
+++ api-BakeHub/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conexión a MongoDB establecida');
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
